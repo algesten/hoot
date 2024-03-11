a27106934c2fb47ee56a32991fa504f90befebed
Martin Algesten <martin@algesten.se>
2024-03-11 21:51:32 +0100

    more handlers

--- usrv/src/handler.rs
+++ usrv/src/handler.rs
@@ -55,42 +55,74 @@ where
     }
 }
 
-impl<S, F, T1, T2, Ret> Handler<((), T1, T2), S> for F
-where
-    F: FnOnce(T1, T2) -> Ret + Clone + Send + 'static,
-    T1: FromRequestRef<S>,
-    T2: FromRequest<S>,
-    Ret: Into<Response>,
-{
-    fn call(self, state: S, request: Request) -> Response {
-        let t1 = match T1::from_request(&state, &request) {
-            Ok(v) => v,
-            Err(e) => return e.into(),
-        };
-        let t2 = match T2::from_request(&state, request) {
-            Ok(v) => v,
-            Err(e) => return e.into(),
-        };
-        (self)(t1, t2).into()
+macro_rules! impl_handler {
+    (
+        [$($ty:ident),*], $last:ident
+    ) => {
+        #[allow(non_snake_case)]
+        impl<S, F, $($ty,)* $last, Ret> Handler<((), $($ty,)* $last), S> for F
+        where
+            F: FnOnce($($ty,)* $last) -> Ret + Clone + Send + 'static,
+            Ret: Into<Response>,
+            $( $ty: FromRequestRef<S> + Send, )*
+            $last: FromRequest<S> + Send,
+        {
+            fn call(self, state: S, request: Request) -> Response {
+
+                $(
+                    let $ty = match <$ty>::from_request(&state, &request) {
+                        Ok(v) => v,
+                        Err(e) => return e.into(),
+                    };
+                )*
+
+                let $last = match $last::from_request(&state, request) {
+                    Ok(v) => v,
+                    Err(e) => return e.into(),
+                };
+
+                (self)($($ty,)* $last).into()
+            }
+        }
+        #[allow(non_snake_case)]
+        impl<S, F, $($ty,)* $last, Ret> Handler<(((),), $($ty,)* $last), S> for F
+        where
+            F: FnOnce(S, $($ty,)* $last) -> Ret + Clone + Send + 'static,
+            Ret: Into<Response>,
+            $( $ty: FromRequestRef<S> + Send, )*
+            $last: FromRequest<S> + Send,
+        {
+            fn call(self, state: S, request: Request) -> Response {
+
+                $(
+                    let $ty = match <$ty>::from_request(&state, &request) {
+                        Ok(v) => v,
+                        Err(e) => return e.into(),
+                    };
+                )*
+
+                let $last = match $last::from_request(&state, request) {
+                    Ok(v) => v,
+                    Err(e) => return e.into(),
+                };
+
+                (self)(state, $($ty,)* $last).into()
+            }
+        }
     }
 }
 
-impl<S, F, T1, T2, Ret> Handler<(((),), T1, T2), S> for F
-where
-    F: FnOnce(S, T1, T2) -> Ret + Clone + Send + 'static,
-    T1: FromRequestRef<S>,
-    T2: FromRequest<S>,
-    Ret: Into<Response>,
-{
-    fn call(self, state: S, request: Request) -> Response {
-        let t1 = match T1::from_request(&state, &request) {
-            Ok(v) => v,
-            Err(e) => return e.into(),
-        };
-        let t2 = match T2::from_request(&state, request) {
-            Ok(v) => v,
-            Err(e) => return e.into(),
-        };
-        (self)(state, t1, t2).into()
-    }
-}
+impl_handler! { [T1], T2 }
+impl_handler! { [T1, T2], T3 }
+impl_handler! { [T1, T2, T3], T4 }
+impl_handler! { [T1, T2, T3, T4], T5 }
+impl_handler! { [T1, T2, T3, T4, T5], T6 }
+impl_handler! { [T1, T2, T3, T4, T5, T6], T7 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7], T8 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T8], T9 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10], T11 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10, T11], T12 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12], T13 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12, T13], T14 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12, T13, T14], T15 }
+impl_handler! { [T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12, T13, T14, T15], T16 }
